b923e82255212e49d1d84a0c583b84b0baee6f49
t-site <sktruntime@hotmail.co.jp>
2024-10-10 12:34:43 +0900

    add for humans communication

--- iu3-handshake.sh
+++ iu3-handshake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LINE=$(. rand.sh iu-handshake.txt 358 )
+LINE=$(. rand.sh iu-handshake.txt 430 )
 head -"${LINE}" iu-handshake.txt | tail -1
 LINE=$(. rand.sh em.txt 256 )
 head -"${LINE}" em.txt | tail -1
